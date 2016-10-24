bbad498c115e9a70387e1bed61f50c325b8ca5d8
Isa Zhang <isazhang15@163.com>
2016-10-25 00:23:29 +0800

    Isa made a few edits but may not work at all

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     path = require('path'),
     $ = require('./lib/utils'),
+    conf = require('./lib/config.js'),
     app = express();
 
 $.mongo.connect($.config.mongo, function(err) {
@@ -21,16 +22,22 @@ function start() {
         extended: false
     }));
 
-    app.use('/wxapi', require('./lib/routers/wxapi'));
+    app.use('/wxapi', require('./lib/routers/wxapi.js'));
+    app.use('/users', require('./lib/routers/users.js'));
+    app.use('/checkin', require('./lib/routers/checkin.js'));
 
     // create menu
     //$.wechat.createMenu(require('./lib/config/menu.json'));
+    $.wechat.createMenu(require('./lib/menu.json'));
+
 
     $.error('=====================================================');
     $.error('==    |-.-|    HUNGER GAME API SERVICE    |-.-|    ==');
     $.error('=====================================================');
     $.error('==    Started at ' + new Date().toUTCString() + '     ==');
     $.error('=====================================================');
+
+
 
     app.listen($.config.port, function(err, b) {
       console.log('running...');
